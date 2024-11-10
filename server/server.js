2fa3f6a6d0c48e342d80191250203a4b4daaec21
Joseph Lang <143908150+joseph-lang7@users.noreply.github.com>
2024-11-09 19:18:39 -0800

    Connect mongodb

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
 
@@ -10,4 +11,5 @@ const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
