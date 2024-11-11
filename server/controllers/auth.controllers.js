b5892dcb9acb99c7fa93c09072ca6b2c23791d88
Joseph Lang <143908150+joseph-lang7@users.noreply.github.com>
2024-11-10 18:46:09 -0800

    Implement sign up api

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,5 +1,16 @@
-export const signup = (req, res) => {
-  res.send("Sign up route called");
+import { User } from "../models/user.model.js";
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = await User.create({ name, email, password });
+    res.status(201).json({ user, message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const login = (req, res) => {
