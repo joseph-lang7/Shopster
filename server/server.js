eeb6dacb99a5208758c1b7e03356d81854e17f4f
Joseph Lang <143908150+joseph-lang7@users.noreply.github.com>
2024-11-12 17:41:10 -0800

    Implement logout api

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3001;
