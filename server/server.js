10474c94023b69d97791910f069fde59509285f3
Joseph <langj6757@gmail.com>
2024-11-22 17:59:07 -0800

    Rename directories and files for consistency

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
+import authRoutes from "./routes/auth.routes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
-import productRoutes from "./routes/product.route.js";
+import productRoutes from "./routes/product.routes.js";
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes)
+app.use("/api/products", productRoutes);
 app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 3001;
@@ -19,4 +19,3 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
-
