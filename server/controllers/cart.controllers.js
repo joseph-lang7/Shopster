9c7cbfce333c450992000e1cfdc035d97547d00f
Joseph <langj6757@gmail.com>
2024-11-25 19:56:08 -0800

    Create update quantity in cart api route

--- server/controllers/cart.controllers.js
+++ server/controllers/cart.controllers.js
@@ -35,3 +35,27 @@ export const removeAllFromCart = async (req, res) => {
       .json({ message: "Server error", error: error.message });
   }
 };
+
+export const updateQuantityInCart = async (req, res) => {
+  try {
+    const { id: productId } = req.params;
+    const { quantity } = req.body;
+    const user = req.user;
+    const existingItem = user.cartItems.find((item) => item.id === productId);
+    if (existingItem) {
+      if (quantity === 0) {
+        user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+        await user.save();
+        return res.json(user.cartItems);
+      }
+
+      existingItem.quantity = quantity;
+      await user.save();
+      res.json(user.cartItems);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal error", error: error.message });
+  }
+};
--- server/routes/cart.routes.js
+++ server/routes/cart.routes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   addToCart,
   removeAllFromCart,
+  updateQuantityInCart,
 } from "../controllers/cart.controllers.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/", protectRoute, addToCart);
 router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantityInCart);
 
 export default router;
