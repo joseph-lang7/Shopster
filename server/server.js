730964de6b63d1870d394829b80b4db6d76c07d9
Joseph <langj6757@gmail.com>
2024-12-04 21:56:04 -0800

    Create checkout session route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 const app = express();
 
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 
 const PORT = process.env.PORT || 3001;
 
